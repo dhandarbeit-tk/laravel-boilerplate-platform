d7279dcd1d433e95e24cefe3e12dda6e057fa54a
Thorsten Krüger <tk@programmierung.deutschehandarbeit.de>
2017-05-11 12:00:29 +0200

    dev

--- app/Eloquent/Models/Model.php
+++ app/Eloquent/Models/Model.php
@@ -4,10 +4,5 @@ namespace App\Eloquent\Models;
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
-    protected $table = 'pages';
 
-    public function getRouteKeyName()
-    {
-        return 'slug';
-    }
 }
